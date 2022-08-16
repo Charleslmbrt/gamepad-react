36ad1fdb0c45d1a85233f0840d6667077d23eafe
Charles Lambert <charles@mbp-de-charles.lan>
2022-08-16 14:21:00 +0200

    add .env

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,23 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrophy } from "@fortawesome/free-solid-svg-icons";
 
 const Home = () => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
+  const API_KEY = process.env.API_KEY;
+
   useEffect(() => {
     const fetchData = async () => {
       try {
+        const response = await axios.get(
+          `https://api.rawg.io/api/games?key=${API_KEY}&dates=2022-08-01,2022-08-31&platforms=18,1,7`
+        );
+        setData(response.data);
+        setIsLoading(false);
+        // console.log(response.data);
       } catch (error) {
         console.log(error.response);
       }
@@ -15,18 +25,44 @@ const Home = () => {
     fetchData();
   }, []);
 
-  return (
+  return isLoading ? (
+    <p>Loading</p>
+  ) : (
     <div className="main-home">
-      <div className="container-home">
-        <div className="container-card">
-          <div className="img-card"></div>
-          <div className="description-card">
-            <div className="platform-card"></div>
-            <div className="title-card"></div>
-            <div className="rate-card"></div>
-            <div className="fav-card"></div>
-          </div>
+      <div class="sidenav">
+        <div className="category-sidebar">
+          <p>Home</p>
         </div>
+        <div className="category-sidebar">
+          <p>Review</p>
+        </div>
+        <div className="category-sidebar">
+          <p>Top</p>
+
+          <a href="#">
+            <FontAwesomeIcon icon={faTrophy} className="icon-sidebar" />
+            Best of the year
+          </a>
+          <a href="#">Popular in 2021</a>
+          <a href="#">All time top 250</a>
+        </div>
+      </div>
+      <div className="container-home">
+        {data.results.map((game, index) => {
+          return (
+            <div key={index} className="container-card">
+              <div className="img-card">
+                <img src={game.background_image} />
+              </div>
+              <div className="description-card">
+                <div className="platform-card"></div>
+                <div className="title-card"></div>
+                <div className="rate-card"></div>
+                <div className="fav-card"></div>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
